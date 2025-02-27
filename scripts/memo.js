70948264aabb78604bbb8404224c619b259fcd48
genistack <genistack@outlook.com>
2025-02-27 17:16:17 +0200

    3 commit

--- scripts/memo.js
+++ scripts/memo.js
@@ -1,5 +1,28 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+    const modal = document.getElementById("addMemoModal");
+    const createMemo = document.getElementById("create-memo");
+    const closeBtn = document.querySelector(".close");
+    const saveBtn = document.getElementById("saveMemo");
+
+    // Open modal
+    createMemo.addEventListener("click", () => {
+      modal.style.display = "flex";
+    });
+
+    // Close modal
+    closeBtn.addEventListener("click", () => {
+      modal.style.display = "none";
+    });
+
+    // Close modal when clicking outside
+    window.addEventListener("click", (e) => {
+      if (e.target === modal) {
+        modal.style.display = "none";
+      }
+    });
+
+
     document.querySelector('#memo-container').style.display = 'none';
 
     document.querySelectorAll(".memo-menu").forEach(button => {
